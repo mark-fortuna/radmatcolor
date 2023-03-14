52e3caab7f44a07dc9a64516ab2919bd87ef56dd
Mark Fortuna <91466996+mark-fortuna@users.noreply.github.com>
2023-03-14 14:19:00 +0100

    material visability added
    Materials labeled with 'invisible' in color dictionary file are made invisible.

--- radmatcolor.py
+++ radmatcolor.py
@@ -3,43 +3,42 @@
 # Script can be run in terminal with arguments:
 # python radmatcolor.py <input_name> <material_color_directory> [<output_name> <additional_changes>]
 
-# TO DO: Some materials should be invisible.
-#        Script should add visability section and make
-#        bodies with invisible materials invisible.
-
 import regex as re
 import os
 import sys
 import csv
 
 ### READING ARGUMENTS IN TERMINAL ###
-if len(sys.argv) == 1:      # no arguments
-    print('\nScript radmatcolor.py recolors your radiant input (.inp file) as described in material color dictionary (.csv file).\nTo run:\npython radmatcolor.py <rad_input> <mat_color_dict> [<output_name>]\nHere arguments inside [] are optional.\nFor help run:\npython radmatcolor.py -h\n')
-    quit()
-elif len(sys.argv) == 2:    # one argument (help or example)
-    arg = sys.argv[1]
-    if arg == '-h' or arg == '-help':
-        print('\nFor alternative script modes use:\npython radmatcolor.py -option\n\nWith options:\n\t-h\tor -help\t for help\n\t-e\tor -example\t for example files\n')
-    elif arg == '-e' or arg == '-example':
-        print('Make example rad.inp and color_dict.csv')
-    quit()
-elif len(sys.argv) > 3:     # specified output name
-    in_name = sys.argv[1]
-    mat_color_dict_name = sys.argv[2]
-    out_name = os.path.splitext(sys.argv[3])[0]
-    specified_out_name = True
+if len(sys.argv) == 1:	  # no arguments
+	print('\nScript radmatcolor.py recolors your radiant input (.inp file) as described in material color dictionary (.csv file).\nTo run:\npython radmatcolor.py <rad_input> <mat_color_dict> [<output_name>]\nHere arguments inside [] are optional.\nFor help run:\npython radmatcolor.py -h\n')
+	quit()
+elif len(sys.argv) == 2:	# one argument (help or example)
+	arg = sys.argv[1]
+	if arg == '-h' or arg == '-help':
+		print('\nFor alternative script modes use:\npython radmatcolor.py -option\n\nWith options:\n\t-h\tor -help\t for help\n\t-e\tor -example\t for example files\n')
+	elif arg == '-e' or arg == '-example':
+		print('Make example rad.inp and color_dict.csv')
+	quit()
+elif len(sys.argv) > 3:	 # specified output name
+	in_name = sys.argv[1]
+	mat_color_dict_name = sys.argv[2]
+	out_name = os.path.splitext(sys.argv[3])[0]
+	specified_out_name = True
 else:
-    in_name = sys.argv[1]   # not specified output name
-    mat_color_dict_name = sys.argv[2]
-    out_name = os.path.splitext(in_name)[0]
-    specified_out_name = False
+	in_name = sys.argv[1]   # not specified output name
+	mat_color_dict_name = sys.argv[2]
+	out_name = os.path.splitext(in_name)[0]
+	specified_out_name = False
 
 ### READING MATERIAL COLOR DICTIONARY FILE ###
 mat_file = open(mat_color_dict_name, mode='r')
 mat_reader = csv.reader(mat_file)
 mat_color = {}
+invisible_materials = []
 for rows in mat_reader:
-    mat_color[int(rows[0])] = f'  {rows[1]}  {rows[2]}  {rows[3]}  {rows[4]}  '
+	if rows[1] == 'invisible':
+		invisible_materials.append(int(rows[0]))
+	else: mat_color[int(rows[0])] = f'  {rows[1]}  {rows[2]}  {rows[3]}  {rows[4]}  '
 
 ### OPENING RADIANT INPUT FILE ###
 in_file = open(in_name, 'r')
@@ -47,45 +46,55 @@ in_lines = in_file.readlines()
 in_file.close()
 
 ### READING MATERIALS IN RADIANT INPUT ###
-line_mat = []       # a list of materials in each line of Radiant input file 
+line_mat = []	   # a list of materials in each line of Radiant input file 
 for i in in_lines:
-    mat = re.findall(r'm(\d+)', i)
-    # print(mat)
-    if len(mat) > 1: print(f'Error: more than one material in line:\n\t{i}')
-    elif len(mat) == 1: line_mat.append(int(mat[0]))
-    else: line_mat.append(None)
+	mat = re.findall(r'm(\d+)', i)
+	# print(mat)
+	if len(mat) > 1: print(f'Error: more than one material in line:\n\t{i}')
+	elif len(mat) == 1: line_mat.append(int(mat[0]))
+	else: line_mat.append(None)
 
 ### MAKING NEW RADIANT INPUT ###
 out_lines = []
+invisible_cells = []
 # Go trough all lines
 for i,v in enumerate(line_mat):
-    if not v:
-        out_lines.append(in_lines[i])               # copy old line if no material is asigned
-        change = 'no material'
-    else:                                           # if material is asigned do more checks
-        if list(mat_color.keys()).count(v) > 0:     # is the material the one we want to change?
-           # we need to change the color code:
-           new_color = mat_color[v]
-           changed_line = re.sub(r'\s+\d+\s+\d+\s+\d+\s+\d+\s+!', f'{new_color}!', in_lines[i])
-           out_lines.append(changed_line)
-           change = f'material {v} has been changed to {new_color}'
-        else:
-            out_lines.append(in_lines[i])
-            change = f"didn't change material {v}"
-    print(f'line {i:03}:\t{change}')
+	if not v:
+		out_lines.append(in_lines[i])			   # copy old line if no material is asigned
+		change = 'no material'
+	else:										   # if material is asigned do more checks
+		if list(mat_color.keys()).count(v) > 0:	 # is the material the one we want to change?
+			# we need to change the color code:
+			new_color = mat_color[v]
+			changed_line = re.sub(r'\s+\d+\s+\d+\s+\d+\s+\d+\s+!', f'{new_color}!', in_lines[i])
+			out_lines.append(changed_line)
+			change = f'material {v} has been changed to {new_color}'
+		elif invisible_materials.count(v) > 0:	# check wether material should be invisible
+			out_lines.append(in_lines[i])
+			cell_number = re.search(r'!\s+\d+', in_lines[i]).group().split(' ')[-1]
+			invisible_cells.append(f'-{cell_number}')
+			change = f'cell {cell_number} has invisible material {v}'
+		else:
+			out_lines.append(in_lines[i])
+			change = f"didn't change material {v}"
+	print(f'line {i:03}:\t{change}')
+
+### MAKING MATERIALS INVISIBLE ###
+mat_vis_text = '\n\nvisibility { \n  materials_off ' + ' '.join(invisible_cells) + ' \n}'
+out_lines.append(mat_vis_text)
 
 ### FILE SAVING ###
 if specified_out_name:
-    out_file = open(f'{out_name}.inp', 'w')
-    out_file.writelines(out_lines)
-    out_file.close()
+	out_file = open(f'{out_name}.inp', 'w')
+	out_file.writelines(out_lines)
+	out_file.close()
 else:
-    def format(i): return f'{out_name}-{i:03}.inp'
-    i = 1
-    is_file = os.path.isfile(format(i))
-    while is_file:
-        i += 1
-        is_file = os.path.isfile(format(i))
-    out_file = open(format(i), 'w')
-    out_file.writelines(out_lines)
-    out_file.close()
+	def format(i): return f'{out_name}-{i:03}.inp'
+	i = 1
+	is_file = os.path.isfile(format(i))
+	while is_file:
+		i += 1
+		is_file = os.path.isfile(format(i))
+	out_file = open(format(i), 'w')
+	out_file.writelines(out_lines)
+	out_file.close()
